2227a37f735daf51e6f5d93f94f55f51700c6ab9
Arda Eren <Poedx2@gmail.com>
2025-05-30 23:12:41 +0300

    add original extensions

--- cstomd.ps1
+++ cstomd.ps1
@@ -2,7 +2,7 @@ $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 Get-ChildItem -Path $scriptDir -Recurse -Filter *.cs | ForEach-Object {
     $originalPath = $_.FullName
-    $newPath = $originalPath -replace '\.cs$', '.md'
+    $newPath = $originalPath -replace '\.cs$', '.cs.md'
 
     # Read the original content
     $content = Get-Content -Raw -Path $originalPath
